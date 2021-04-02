76ec3adcf30549ac946b32f7aeaf64492bd9bdd4
jrodley <john@rodley.com>
2021-04-02 18:04:42 -0400

    reorder actions put grpc first

--- sense-go/main.go
+++ sense-go/main.go
@@ -14,12 +14,9 @@ import (
 	"github.com/go-playground/log"
 	"github.com/go-stomp/stomp"
 	hc "github.com/jdevelop/golang-rpi-extras/sensor_hcsr04"
-	"gobot.io/x/gobot"
-	"gobot.io/x/gobot/drivers/i2c"
 	"gobot.io/x/gobot/platforms/raspi"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	"math"
 	"os"
 	"strings"
 	"sync"
@@ -32,67 +29,6 @@ var BubblesnetVersionPatchString = ""
 var BubblesnetBuildNumberString = ""
 var BubblesnetBuildTimestamp = ""
 var BubblesnetGitHash = ""
-
-func runTamperDetector() {
-	log.Info("runTamperDetector")
-	adxl345Adaptor := raspi.NewAdaptor()
-	adxl345 := i2c.NewADXL345Driver(adxl345Adaptor)
-	lastx := 0.0
-	lasty := 0.0
-	lastz := 0.0
-
-	xmove := 0.0
-	ymove := 0.0
-	zmove := 0.0
-
-	work := func() {
-		gobot.Every(100*time.Millisecond, func() {
-			x, y, z, _ := adxl345.XYZ()
-			//			log.Debugf("x: %.7f | y: %.7f | z: %.7f \n", x, y, z))
-			if lastx == 0.0 {
-			} else {
-				xmove = math.Abs(lastx - x)
-				ymove = math.Abs(lasty - y)
-				zmove = math.Abs(lastz - z)
-				if xmove > globals.MyStation.TamperSpec.Xmove ||  ymove > globals.MyStation.TamperSpec.Ymove ||  zmove > globals.MyStation.TamperSpec.Zmove {
-					log.Infof("new tamper message !! x: %.3f | y: %.3f | z: %.3f ", xmove, ymove, zmove)
-					var tamperMessage = messaging.NewTamperSensorMessage("tamper_sensor",
-						0.0, "", "", xmove, ymove, zmove)
-					bytearray, err := json.Marshal(tamperMessage)
-					if err != nil {
-						fmt.Println(err)
-						return
-					}
-					message := pb.SensorRequest{Sequence: globals.GetSequence(), TypeId: "sensor", Data: string(bytearray)}
-					_, err = globals.Client.StoreAndForward(context.Background(), &message)
-					if err != nil {
-						log.Errorf("runTamperDetector ERROR %v", err)
-					} else {
-//						log.Debugf("%v", sensor_reply)
-					}
-
-				} else {
-					//					log.Debugf("x: %.3f | y: %.3f | z: %.3f \n", xmove, ymove, zmove))
-				}
-			}
-			lastx = x
-			lasty = y
-			lastz = z
-		})
-	}
-
-	robot := gobot.NewRobot("adxl345Bot",
-		[]gobot.Connection{adxl345Adaptor},
-		[]gobot.Device{adxl345},
-		work,
-	)
-
-	err := robot.Start()
-	if err != nil {
-		globals.ReportDeviceFailed("adxl345")
-		log.Errorf("adxl345 robot start error %v", err)
-	}
-}
 
 var lastDistance = float64(0.0)
 
@@ -497,7 +433,7 @@ func main() {
 	log.Info("movement")
 	if moduleShouldBeHere(globals.ContainerName, globals.MyDevice.DeviceID, globals.MyStation.MovementSensor, "adxl345") {
 		log.Info("MovementSensor should be connected to this device, starting")
-		go runTamperDetector()
+		go RunTamperDetector()
 	} else {
 		log.Warnf("No adxl345 Configured - skipping tamper detection")
 	}
--- sense-go/main_test.go
+++ sense-go/main_test.go
@@ -632,7 +632,7 @@ func Test_runTamperDetector(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			runTamperDetector()
+			RunTamperDetector()
 		})
 	}
 }
